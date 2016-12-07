84e8f0101411e5b510be22d81b4b6c7af9f480ca
Brian Gibbins <brian@briangibbins.com>
2016-12-07 14:55:42 -0500

    nothing is ever perfect

--- presentation/javascript.js
+++ presentation/javascript.js
@@ -2,5 +2,5 @@
  * Created by briangibbins on 12/3/16.
  */
 function showModem() {
-    document.getElementById("IMG1").src = "img_modem.jpg";
+    document.getElementById("IMG1").src = "img_javascript.png";
 }
